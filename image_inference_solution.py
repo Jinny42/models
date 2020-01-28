b088cdb94409548e8b0ee53bfad0171bbee5e2cc
Jinny42 <47097413+Jinny42@users.noreply.github.com>
2020-01-28 16:04:52 +0900

    Update image_inference_solution.py

--- image_inference_solution.py
+++ image_inference_solution.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
-IMAGE_PATH = '/home/mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/test.png'
-PB_PATH = '/home/mds/models/frozen_inference_graph.pb'
+IMAGE_PATH = '/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/test.png'
+PB_PATH = '/home/opencv-mds/models/frozen_inference_graph.pb'
 
 def import_graph(PATH_TO_FTOZEN_PB):
     detection_graph = tf.Graph()
